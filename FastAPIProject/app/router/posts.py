cc4f75d85593af5874e46bb8db8c660b04c07c0b
1Mashilo <christophermashilo@gmail.com>
2023-12-21 15:25:16 +0100

    removed unused imprt and added docstrings to functions for better documentation

--- FastAPIProject/app/router/posts.py
+++ FastAPIProject/app/router/posts.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI, HTTPException, Response, status, Depends, APIRouter
-from sqlalchemy.orm import Session
-from app.models import Base, Post, User
-from app.schemas import PostCreate, UserCreate, UserOut, PostResponse
-from app.database import engine, get_db
-from app.utils import sqlalchemy_model_to_dict
-from .auth import oauth2, get_current_user
 from typing import Optional, List
+from fastapi import HTTPException, Response, status, Depends, APIRouter
+from sqlalchemy.orm import Session
+from app.models import Post, User
+from app.schemas import PostCreate, PostResponse
+from app.database import get_db
+from .auth import get_current_user
 
 router = APIRouter()
+ 
 
 @router.get("/posts", response_model=List[PostResponse])
 def get_posts(
@@ -18,6 +18,7 @@ def get_posts(
     category: Optional[str] = "",
     author: Optional[str] = ""
 ):
+    """Endpoint to retrieve a list of posts."""
     query = db.query(Post).filter(Post.title.contains(search))
 
     if category:
@@ -27,18 +28,8 @@ def get_posts(
         query = query.filter(Post.author == author)
 
     posts = query.limit(limit).offset(skip).all()
-      # Convert SQLAlchemy models to Pydantic models
-    response_posts = []
-    for post in posts:
-        owner_pydantic = UserOut.from_orm(post.owner)
-        response_post = PostResponse(
-            **sqlalchemy_model_to_dict(post),
-            owner=owner_pydantic
-        )
-        response_posts.append(response_post)
 
-    return response_posts
-    
+    return posts
 
 
 @router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=PostResponse)
@@ -47,34 +38,25 @@ def create_post(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
+    """Endpoint to create a new post."""
     db_post = Post(**post.dict(), owner_id=current_user.id)
     db.add(db_post)
     db.commit()
     db.refresh(db_post)
 
-    # Convert SQLAlchemy model instance to Pydantic model
-    owner_pydantic = UserOut.from_orm(current_user)
-
-    # Populate the owner field in the response
-    response_post = PostResponse(
-        **sqlalchemy_model_to_dict(db_post),
-        owner=owner_pydantic
-    )
-
-    return response_post
+    return db_post
 
 
 @router.get("/posts/{id}", response_model=PostResponse)
 def get_post(id: int, db: Session = Depends(get_db)):
+    """Endpoint to retrieve a specific post by ID."""
     db_post = db.query(Post).filter(Post.id == id).first()
 
     if not db_post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id: {id} was not found"
-        )
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not found")
 
-    return sqlalchemy_model_to_dict(db_post)
+    return db_post
+
 
 @router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(
@@ -82,21 +64,20 @@ def delete_post(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
+    """Endpoint to delete a post."""
     db_post = db.query(Post).filter(Post.id == id).first()
 
     if not db_post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id: {id} does not exist"
-        )
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} does not exist")
 
     if db_post.owner_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform the requested action")
 
     db.delete(db_post)
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 @router.put("/posts/{id}", response_model=PostResponse)
 def update_post(
@@ -105,26 +86,19 @@ def update_post(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
+    """Endpoint to update a post."""
     db_post = db.query(Post).filter(Post.id == id).first()
 
     if not db_post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id: {id} does not exist"
-        )
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} does not exist")
 
     if db_post.owner_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform the requested action")
 
     for key, value in post.dict().items():
         setattr(db_post, key, value)
 
     db.commit()
     db.refresh(db_post)
-    owner_pydantic = UserOut.from_orm(db_post.owner)
-    response_post = PostResponse(
-        **sqlalchemy_model_to_dict(db_post),
-        owner=owner_pydantic
-    )
 
-    return response_post
+    return db_post
